576a0a453cd8072bcc0ef9118aa7d3daf39bfb10
holishing <holishingtest@gmail.com>
2019-10-15 11:06:18 +0800

    CI: try build matrix

--- run_docker_build.sh
+++ run_docker_build.sh
@@ -2,6 +2,7 @@
 
 set -ex
 test -z "$APPVEYOR" && APPVEYOR=0
+test -z "$DEBVER"   && DEBVER=buster
 set -u
 
 ### This shell script aim to be a POSIX-shell compatible script
@@ -11,21 +12,14 @@ docker version                && printf "\033[1;32mDocker Commands Exist!\033[0m
 (id|grep docker >> /dev/null) && printf "\033[1;32mWe can run docker without root permission!\033[0m\n"
 
 cd $(dirname "$0")  ## check runtime path is in what the script located in
-docker build --no-cache -t "holishing/ffmpeg-static-env:buster" -f Dockerfile .
-docker tag "holishing/ffmpeg-static-env:buster" "holishing/ffmpeg-static-env:linux3.2.0"
-docker tag "holishing/ffmpeg-static-env:buster" "holishing/ffmpeg-static-env:latest"
-docker build --no-cache --build-arg "DEBVER=stretch" -t "holishing/ffmpeg-static-env:stretch" -f Dockerfile .
-docker tag "holishing/ffmpeg-static-env:stretch" "holishing/ffmpeg-static-env:linux2.6.32"
+docker build --no-cache --build-arg DEBVER="$DEBVER" -t holishing/ffmpeg-static-env:"$DEBVER" .
+printf "\033[1;32m=================Build Environment Set up!=================\033[0m\n"
 
-mkdir -p build/linux3 build/linux2
-CONTAINER_LINUX3=container_linux3
-CONTAINER_LINUX2=container_linux2
+CONTAINER_LINUX=container_"$DEBVER"
 test -eq $APPVEYOR 0 && DOCKER_TERMIO_ARG=" -it" || DOCKER_TERMIO_ARG=""
+docker run $DOCKER_TERMIO_ARG --name "$CONTAINER_LINUX" holishing/ffmpeg-static-env:"$DEBVER" ./build.sh || docker rm "$CONTAINER_LINUX"
 
-docker run $DOCKER_TERMIO_ARG --name "$CONTAINER_LINUX3" "holishing/ffmpeg-static-env:linux3.2.0"  ./build.sh || docker rm "$CONTAINER_LINUX3"
-docker cp "$CONTAINER_LINUX3":/ffmpeg-static/bin build/linux3 && docker rm "$CONTAINER_LINUX3"
-
-docker run $DOCKER_TERMIO_ARG --name "$CONTAINER_LINUX2" "holishing/ffmpeg-static-env:linux2.6.32" ./build.sh || docker rm "$CONTAINER_LINUX2"
-docker cp "$CONTAINER_LINUX2":/ffmpeg-static/bin build/linux2 && docker rm "$CONTAINER_LINUX2"
+mkdir -p build/"$DEBVER"
+docker cp "$CONTAINER_LINUX":/ffmpeg-static/bin build/"$DEBVER" && docker rm "$CONTAINER_LINUX"
 
 printf "\033[1;32m===================Completed!!!====================\033[0m\n"
